4f9fa825eeb61dc040f2ffec97a8ded486e9a84b
benjamin0927 <benjamin0927@yeah.net>
2015-04-03 07:49:04 +0800

    Add poi example dependency

---
+++ maven.excel/src/main/java/org/benjamin/excel/poi/ExcelOperation.java
@@ -0,0 +1,14 @@
+package org.benjamin.excel.poi;
+
+/**
+ * http://www.cnblogs.com/hongten/archive/2012/02/22/java2poi.html
+ *
+ */
+
+
+public class ExcelOperation {
+
+	public static void main(String[] args) {
+	}
+
+}
