6cb2704f205871628614523a9c4a7ad0af77c574
benjamin0927 <benjamin0927@yeah.net>
2015-04-10 07:29:58 +0800

    Add useless debug log info

--- junit/src/org/benjamin/log/appender/LogAppenderConsole.java
+++ junit/src/org/benjamin/log/appender/LogAppenderConsole.java
@@ -6,10 +6,6 @@ import org.benjamin.log.LogLevel;
 
 public class LogAppenderConsole extends LogAppender {
 	PrintStream writer = System.out;
-
-	public LogAppenderConsole(){
-		writer.println( " LogAppenderConsole - " );
-	}
 	@Override
 	public void log(LogLevel logLevel, String message) {
 		writer.println(logLevel + " - " + message);
